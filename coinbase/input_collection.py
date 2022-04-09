1b0ee72c1c88bbdf436e0542fd3cec21ae977fbf
Andrew Ghorbani <andrewbghorbani@gmail.com>
2022-04-09 00:30:43 -0700

    Creating unit tests for InputCollector()

--- coinbase/input_collection.py
+++ coinbase/input_collection.py
@@ -1,13 +1,56 @@
+from pickle import FALSE
 import requests
 
 from coinbase.frequency import FREQUENCY_TO_DAYS
 from datetime import datetime
 
 
+BASE_URL = "https://api.exchange.coinbase.com/currencies/"
+
+
 class InputCollector():
 
     def __init__(self):
         self.start_date_is_today = False
+
+    def is_valid_start_date(self, date_string):
+        """
+        Checks if provided date string is valid.
+
+        :param date_string: str
+        :return: True if valid; False otherwise
+        """
+
+        if not isinstance(date_string, str):
+            return False
+
+        # Null check
+        if not date_string:
+            print("Date cannot be null.")
+            return False
+
+        # Make sure it's a valid date
+        try:
+            format = "%Y-%m-%d"
+            converted_date = datetime.strptime(date_string, format)
+
+        except ValueError:
+            print("This date does not exist!")
+            return False
+
+        # Make sure the date isn't before the current day
+        current_date = datetime.now().replace(hour=0, minute=0, 
+                                                second=0, microsecond=0)
+
+        if converted_date < current_date:
+            print("The date must occur on or after the current date.")
+            return False
+
+        # Need to know if today is the start date for time validation
+        if converted_date == current_date:
+            self.start_date_is_today = True
+
+        return True
 
     def get_start_date(self):
         """
@@ -21,36 +64,47 @@ class InputCollector():
         while not valid_date:
 
             date = input("Enter in the start date in format YYYY-MM-DD: ")
-
-            # Null check
-            if not date:
-                print("Date cannot be null.")
-                continue
-
-            # Make sure it's a valid date
-            try:
-                format = "%Y-%m-%d"
-                converted_date = datetime.strptime(date, format)
-
-            except ValueError:
-                print("This date does not exist!")
-                continue
-
-            # Make sure the date isn't before the current day
-            current_date = datetime.now().replace(hour=0, minute=0, 
-                                                  second=0, microsecond=0)
-
-            if converted_date < current_date:
-                print("The date must occur on or after the current date.")
-                continue
-
-            # Need to know if today is the start date for time validation
-            if converted_date == current_date:
-                self.start_date_is_today = True
-
-            valid_date = True
+            valid_date = self.is_valid_start_date(date)
 
         return date
+
+    def is_valid_start_time(self, time_string):
+        """
+        Checks if provided time string is valid.
+
+        :param date_string: str
+        :return: True if valid; False otherwise
+        """
+
+        if not isinstance(time_string, str):
+            return False
+
+        # Null check
+        if not time_string:
+            print("Time cannot be null.")
+            return False
+
+        # Make sure its a valid time
+        try:
+            format = "%I:%M %p"
+            converted_time = datetime.strptime(time_string, format).time()
+
+        except ValueError:
+            print("This is not a valid time.")
+            return False
+
+        # If the start date is today, the time must occur after the current time.
+        if self.start_date_is_today:
+
+            current_time = datetime.now().time()
+
+            if converted_time < current_time:
+                print(
+                    "The chosen start date is today. The time of the transaction must occur after the current time."
+                )
+                return False
+
+        return True
 
     def get_start_time(self):
         """
@@ -63,38 +117,37 @@ class InputCollector():
 
         while not valid_start_time:
 
-            start_time = input(
-                "Enter in the time you wish to conduct transactions in format HH:MM XM: "
-            )
+            start_time = \
+                input("Enter in the time you wish to conduct transactions in format HH:MM XM: ")
 
-            # Null check
-            if not start_time:
-                raise TypeError("Time cannot be null.")
-                continue
-
-            # Make sure its a valid time
-            try:
-                format = "%I:%M %p"
-                converted_time = datetime.strptime(start_time, format).time()
-
-            except ValueError:
-                print("This is not a valid time.")
-                continue
-
-            # If the start date is today, the time must occur after the current time.
-            if self.start_date_is_today:
-
-                current_time = datetime.now().time()
-
-                if converted_time < current_time:
-                    print(
-                        "The chosen start date is today. The time of the transaction must occur after the current time."
-                    )
-                    continue
-
-            valid_start_time = True
+            valid_start_time = self.is_valid_start_time(start_time)
 
         return start_time
+
+    def is_valid_frequency(self, frequency):
+        """
+        Checks if provided frequency is valid.
+
+        :param frequency: str
+        :return: True if valid; False otherwise
+        """
+
+        if not isinstance(frequency, str):
+            return False
+
+        # Null check
+        if not frequency:
+            print("Frequency cannot be null.")
+            return False
+
+        # Check frequency is valid
+        if frequency.lower() not in FREQUENCY_TO_DAYS:
+            print(
+                'Invalid value. Valid values include "daily", "weekly", "biweekly", and "monthly".'
+            )
+            return False
+
+        return True
 
     def get_frequency(self):
         """
@@ -111,21 +164,65 @@ class InputCollector():
                 'How often would you like to make purchases? Valid values include "daily", "weekly", "biweekly", and "monthly": '
             )
 
-            # Null check
-            if not frequency:
-                print("Frequency cannot be null.")
-                continue
-
-            # Check frequency is valid
-            if frequency.lower() in FREQUENCY_TO_DAYS:
-                valid_frequency = True
-
-            else:
-                print(
-                    'Invalid value. Valid values include "daily", "weekly", "biweekly", and "monthly".'
-                )
+            valid_frequency = self.is_valid_frequency(frequency)
 
         return frequency
+
+    def is_valid_crypto(self, crypto):
+        """
+        Checks if provided crypto string is valid.
+
+        :param crypto: str
+        :return: True if valid; False otherwise
+        """
+
+        if not isinstance(crypto, str):
+            return False
+
+        # Null check
+        if not crypto:
+            print("Crypto symbol cannot be null.")
+            return False
+
+        if not crypto.isalpha():
+            return False
+
+        crypto = crypto.upper()
+        
+        # Check if the API supports the inputted crypto.
+        r = requests.get(BASE_URL + crypto)
+        if r.status_code != 200:
+            print("Invalid crypto symbol.")
+            return False
+
+        return True
+
+
+    def is_valid_dollar_amount(self, dollar_amount):
+        """
+        Checks if the dollar amount string is valid.
+
+        :param dollar_amount: str
+        :return: True if valid; False otherwise
+        """
+
+        if not isinstance(dollar_amount, str):
+            return False
+
+        # Null check
+        if not dollar_amount:
+            print("Dollar amount cannot be null.")
+            return False
+
+        # Check for value errors
+        try:
+            float(dollar_amount)
+
+        except ValueError:
+            print("The dollar amount must be a numerical value.")
+            return False
+
+        return True
 
     def get_orders(self):
         """
@@ -134,11 +231,10 @@ class InputCollector():
         :return: Dict with key-value pair "crypto" : dollar_amount
         """
 
-        valid_orders = False
-        base_url = "https://api.exchange.coinbase.com/currencies/"
+        keep_ordering = True
         orders = {}
 
-        while not valid_orders:
+        while keep_ordering:
 
             # Grab the crypto symbol first
             valid_crypto = False
@@ -147,29 +243,17 @@ class InputCollector():
                 crypto = input(
                     "Enter in the cryprocurrency symbol you wish to purchase: "
                 )
-                crypto = crypto.upper()
 
-                # There is already a pending order for the inputted cryptocurrency
-                if crypto in orders:
-                    overwrite_order = input(
-                        "There is already a pending order for this cryptocurrency. Continuing will overwrite the previous order. Continue? Y/N "
-                    )
+                valid_crypto = self.is_valid_crypto(crypto)
 
-                    if overwrite_order == "N":
-                        continue
+            # There is already a pending order for the inputted cryptocurrency
+            if crypto in orders:
+                overwrite_order = input(
+                    "There is already a pending order for this cryptocurrency. Continuing will overwrite the previous order. Continue? Y/N "
+                )
 
-                # Null check
-                if not crypto:
-                    print("Crypto symbol cannot be null.")
+                if overwrite_order == "N" or overwrite_order == "n":
                     continue
-
-                # Check if the API supports the inputted crypto.
-                r = requests.get(base_url + crypto)
-                if r.status_code != 200:
-                    print("Invalid crypto symbol.")
-                    continue
-
-                valid_crypto = True
 
             # Next grab the dollar amount
             valid_dollar_amount = False
@@ -177,25 +261,16 @@ class InputCollector():
             while not valid_dollar_amount:
                 dollar_amount = input("Enter in the amount in USD to purchase with: ")
 
-                # Null check
-                if not dollar_amount:
-                    print("Dollar amount cannot be null.")
-                    continue
+                valid_dollar_amount = self.is_valid_dollar_amount(dollar_amount)
+            
+            # Add order into dictionary
+            orders[crypto] = float(dollar_amount)
 
-                # Check for value errors
-                try:
-                    orders[crypto] = float(dollar_amount)
-
-                except ValueError:
-                    print("The dollar amount must be a numerical value.")
-                    continue
-
-                valid_dollar_amount = True
-
+            # Continue adding orders if requested
             wish_to_continue = input("Do you wish to add more orders? Y/N ")
 
-            if wish_to_continue == "N":
-                valid_orders = True
+            if wish_to_continue == "N" or wish_to_continue == "n":
+                keep_ordering = False
 
         return orders
 
--- tests/test_input_collection.py
+++ tests/test_input_collection.py
@@ -5,14 +5,133 @@ from coinbase.input_collection import InputCollector
 
 class TestInputCollector():
 
-    def test_get_start_date(self):
-        pass
+    input_collector = InputCollector()
 
-    def test_get_start_time(self):
-        pass
+    @pytest.mark.parametrize(
+        "date_string,expected",
+        [
+            ("", False),
+            (-1, False),
+            ("banana", False),
+            (100.00, False),
+            ("2022-02-30", False),
+            ("2021-01-01", False),
+            ("01-01-2022", False),
+            ("01/01/2022", False),
+            ("2022-06-15", True),
+            ("2023-01-01", True),
+        ]
+    )
+    def test_is_valid_start_date(self, date_string, expected):
+        """Tests if the given start date is valid."""
 
-    def test_get_frequency(self):
-        pass
-    
-    def test_get_orders(self):
-        pass
+        result = self.input_collector.is_valid_start_date(date_string)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "time_string,expected",
+        [
+            ("", False),
+            (-1, False),
+            ("banana", False),
+            ("19:45pm", False),
+            ("10:00am", False),
+            ("10:00 am", True),
+            ("10:00 AM", True),
+            ("5:45 PM", True),
+            ("05:45 PM", True),
+            ("06:30 am", True),
+        ]
+    )
+    def test_is_valid_start_time_if_start_date_is_today(self, time_string, expected):
+        """Tests if the input is a valid start time if the start date is today."""
+
+        self.input_collector.start_date_is_today = True
+        result = self.input_collector.is_valid_start_time(time_string)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "time_string,expected",
+        [
+            ("", False),
+            (-1, False),
+            ("banana", False),
+            ("19:45pm", False),
+            ("10:00am", False),
+            ("10:00 am", True),
+            ("10:00 AM", True),
+            ("5:45 PM", True),
+            ("05:45 PM", True),
+            ("06:30 am", True),
+        ]
+    )
+    def test_is_valid_start_time_if_start_date_is_not_today(self, time_string, expected):
+        """Tests if the input is a valid start time if the start date is not today."""
+                
+        self.input_collector.start_date_is_today = False
+        result = self.input_collector.is_valid_start_time(time_string)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "frequency,expected",
+        [
+            ("", False),
+            (-1, False),
+            ("2021-01-01", False),
+            ("10:00 AM", False),
+            ("daily", True),
+            ("weekly", True),
+            ("biweekly", True),
+            ("monthly", True),
+            ("biannually", False),
+            ("annually", False)
+        ]
+    )
+    def test_is_valid_frequency(self, frequency, expected):
+        """Tests if the input is a valid frequency."""
+
+        result = self.input_collector.is_valid_frequency(frequency)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "crypto,expected",
+        [
+            ("BTC", True),
+            (-1, False),
+            ("2021-01-01", False),
+            ("10:00 AM", False),
+            ("daily", False),
+            ("ETH", True),
+            ("ADA", True),
+            ("SHIB", True),
+            ("?!?", False),
+            ("123", False)
+        ]
+    )
+    def test_is_valid_crypto(self, crypto, expected):
+        """Tests if the input is a valid frequency."""
+
+        result = self.input_collector.is_valid_crypto(crypto)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "amount,expected",
+        [
+            ("BTC", False),
+            (-1, False),
+            ("2021-01-01", False),
+            ("10:00 AM", False),
+            ("5.00", True),
+            ("$10", False),
+            ("10", True),
+            (10, False),
+            ("?!?", False),
+            ("123abc", False)
+        ]
+    )
+    def test_is_valid_dollar_amount(self, amount, expected):
+        """Tests if the input is a valid dollar amount."""
+
+        result = self.input_collector.is_valid_dollar_amount(amount)
+        assert result == expected
+        
